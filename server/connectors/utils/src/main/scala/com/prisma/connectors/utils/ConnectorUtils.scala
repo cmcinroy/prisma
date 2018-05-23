be7cf5b672aa0e8a5f14e71c7280500e2e86d7a1
Craig McInroy <craig.mcinroy@itacit.com>
2018-05-23 05:34:04 -0300

    First cut Oracle-ized Postgres connector.

--- server/connectors/utils/src/main/scala/com/prisma/connectors/utils/ConnectorUtils.scala
+++ server/connectors/utils/src/main/scala/com/prisma/connectors/utils/ConnectorUtils.scala
@@ -2,10 +2,12 @@ package com.prisma.connectors.utils
 
 import com.prisma.api.connector.ApiConnector
 import com.prisma.api.connector.mysql.MySqlApiConnector
+import com.prisma.api.connector.oracle.{PassiveOracleApiConnector, OracleApiConnector}
 import com.prisma.api.connector.postgresql.{PassivePostgresApiConnector, PostgresApiConnector}
 import com.prisma.config.PrismaConfig
 import com.prisma.deploy.connector.DeployConnector
 import com.prisma.deploy.connector.mysql.MySqlDeployConnector
+import com.prisma.deploy.connector.oracle.{PassiveOracleDeployConnector, OracleDeployConnector}
 import com.prisma.deploy.connector.postgresql.{PassivePostgresDeployConnector, PostgresDeployConnector}
 
 import scala.concurrent.ExecutionContext
@@ -16,6 +18,8 @@ object ConnectorUtils {
     (databaseConfig.connector, databaseConfig.active) match {
       case ("mysql", true)     => MySqlApiConnector(databaseConfig)
       case ("mysql", false)    => sys.error("There is not passive mysql deploy connector yet!")
+      case ("oracle", true)  => OracleApiConnector(databaseConfig)
+      case ("oracle", false) => PassiveOracleApiConnector(databaseConfig)
       case ("postgres", true)  => PostgresApiConnector(databaseConfig)
       case ("postgres", false) => PassivePostgresApiConnector(databaseConfig)
       case (conn, _)           => sys.error(s"Unknown connector $conn")
@@ -27,6 +31,8 @@ object ConnectorUtils {
     (databaseConfig.connector, databaseConfig.active) match {
       case ("mysql", true)     => MySqlDeployConnector(databaseConfig)
       case ("mysql", false)    => sys.error("There is not passive mysql deploy connector yet!")
+      case ("oracle", true)  => OracleDeployConnector(databaseConfig)
+      case ("oracle", false) => new PassiveOracleDeployConnector(databaseConfig)
       case ("postgres", true)  => PostgresDeployConnector(databaseConfig)
       case ("postgres", false) => new PassivePostgresDeployConnector(databaseConfig)
       case (conn, _)           => sys.error(s"Unknown connector $conn")
